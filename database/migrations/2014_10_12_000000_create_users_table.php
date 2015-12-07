30e471e7bcd723be0f4194d4dbd96ab57f44ea4a
phamkykhoi <daotaolaptrinhsuntech@gmail.com>
2015-12-08 05:54:46 +0700

    Login By Facebook

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,24 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            //Info user default
             $table->increments('id');
-            $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('avatar');
+            $table->string('age');
+            $table->string('birthday');
+            $table->boolean('gender');
             $table->string('password', 60);
+            $table->string('registration_type', 32);
+            //Info user register facebook
+            $table->string('facebook_id', 100)->unique();
+            $table->string('facebook_token', 100);
+            $table->string('facebook_name', 200);
+            $table->string('facebook_email', 200)->unique();
+            $table->string('facebook_phone', 60);
+            $table->string('facebook_avatar', 200);
             $table->rememberToken();
             $table->timestamps();
         });
